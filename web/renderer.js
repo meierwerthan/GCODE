d5de0e0479bf00935c03425b3f244bfcf2800207
Jeremy Herrman <jeremy@jherrman.com>
2012-09-11 00:38:31 -0400

    Cleanup, starting to remove jquery dependency.

--- web/renderer.js
+++ web/renderer.js
@@ -1,38 +1,41 @@
-function createScene(element) {
+function createScene(container) {
+  var containerWidth  = container.offsetWidth,
+      containerHeight = container.offsetHeight;
+
+  var camera, controls, scene, renderer;
 
   // Renderer
-  var renderer = new THREE.WebGLRenderer({clearColor:0x000000, clearAlpha: 1});
-  renderer.setSize(element.width(), element.height());
-  element.append(renderer.domElement);
+  renderer = new THREE.WebGLRenderer( { clearColor: 0x000000, clearAlpha: 1, antialias: false } );
+  renderer.setSize(containerWidth, containerHeight);
+  renderer.autoClear = false;
+
+  container.appendChild(renderer.domElement);
   renderer.clear();
 
-  // Scene
-  var scene = new THREE.Scene(); 
+  scene = new THREE.Scene();
 
   // Lights...
-  [[0,0,1,  0xFFFFCC],
-   [0,1,0,  0xFFCCFF],
-   [1,0,0,  0xCCFFFF],
-   [0,0,-1, 0xCCCCFF],
-   [0,-1,0, 0xCCFFCC],
-   [-1,0,0, 0xFFCCCC]].forEach(function(position) {
-    var light = new THREE.DirectionalLight(position[3]);
-    light.position.set(position[0], position[1], position[2]).normalize();
-    scene.add(light);
-  });
+  // [[ 0, 0, 1, 0xFFFFCC],
+  //  [ 0, 1, 0, 0xFFCCFF],
+  //  [ 1, 0, 0, 0xCCFFFF],
+  //  [ 0, 0,-1, 0xCCCCFF],
+  //  [ 0,-1, 0, 0xCCFFCC],
+  //  [-1, 0, 0, 0xFFCCCC]].forEach(function(position) {
+  //   var light = new THREE.DirectionalLight(position[3]);
+  //   light.position.set(position[0], position[1], position[2]).normalize();
+  //   scene.add(light);
+  // });
 
   // Camera...
   var fov    = 45,
-      aspect = element.width() / element.height(),
+      aspect = containerWidth / containerHeight,
       near   = 1,
       far    = 10000,
       camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  //camera.rotationAutoUpdate = true;
-  //camera.position.x = 0;
-  //camera.position.y = 500;
   camera.position.z = 300;
-  //camera.lookAt(scene.position);
+
   scene.add(camera);
+
   controls = new THREE.TrackballControls(camera);
   controls.dynamicDampingFactor = 0.15;
 
@@ -43,15 +46,21 @@ function createScene(element) {
 
     requestAnimationFrame(render); // And repeat...
   }
+
   render();
+
+  function setSize(width, height) {
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
+
+    controls.handleResize();
+  }
 
   // Fix coordinates up if window is resized.
   $(window).on('resize', function() {
-    renderer.setSize(element.width(), element.height());
-    camera.aspect = element.width() / element.height();
-    camera.updateProjectionMatrix();
-    controls.screen.width = window.innerWidth;
-    controls.screen.height = window.innerHeight;
+    setSize(container.offsetWidth, container.offsetHeight);
   });
 
   return scene;
